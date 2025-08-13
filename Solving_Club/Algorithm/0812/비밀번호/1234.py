af38937094aa38f237fffa6a40b78e902997d0c0
KHJune <wnsk0524@naver.com>
2025-08-13 09:02:48 +0900

    0813

--- Solving_Club/Algorithm/0812/비밀번호/1234.py
+++ Solving_Club/Algorithm/0812/비밀번호/1234.py
@@ -8,7 +8,7 @@ for tc in range(1, T + 1):
     length, arr = input().split()
 
     stack = []
-    for i in range(length):
+    for i in range(int(length)):
         if stack and stack[-1] == arr[i]:
             stack.pop()
         else:
