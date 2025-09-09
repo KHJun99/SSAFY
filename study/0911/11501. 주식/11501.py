a84c0bece2fb9c658450d6a2551ae34cd6db680a
KHJun99 <wnsk0524@naver.com>
2025-09-10 01:49:41 +0900

    0909

--- study/0911/11501. 주식/11501.py
+++ study/0911/11501. 주식/11501.py
@@ -22,11 +22,23 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-
+# 이익을 구하는 함수
 def stock(lst):
     global N
-    max_price = float('-inf')
+    # lst의 마지막 인덱스를 최대값으로 가정
+    max_price = lst[N - 1]
+    price = 0
 
+    for idx in range(N - 1, -1, -1):
+        # max_price보다 리스트값이 작으면 이익 발생
+        if max_price > lst[idx]:
+            diff = max_price - lst[idx]
+            price += diff
+        # 이익이 발생할 수 없으므로 최대값 갱신
+        else:
+            max_price = lst[idx]
+
+    return price
 
 
 T = int(input())
@@ -35,5 +47,5 @@ for tc in range(T):
     N = int(input())
     prices = list(map(int, input().split()))
 
-
-
+    total = stock(prices)
+    print(total)
