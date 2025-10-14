dc23751dfd9b2913739c6c429935fd9ee9c71377
KHJun99 <wnsk0524@naver.com>
2025-10-15 02:06:39 +0900

    study_1016

---
+++ study/1016/34559_건물_측량/34559.py
@@ -0,0 +1,48 @@
+# 백준_34559_건물_측량 (G3)
+"""
+## [문제 정리]
+- 관우는 땅과 건물이 그려져 있는 N x M 크기의 지도를 가지고 있다.
+    - 지도의 가장 왼쪽 위 : (1, 1)
+    - 지도의 가장 오른쪽 아래 : (N, M)
+- 지도에서 건물을 표시하는 방법
+    - 1로 표현된 칸
+    - 0으로 표현된 칸 중에서 상하좌우 인접한 0으로 이동해 지도의 테두리에 도달할 수 없는 칸
+- 지도의 테두리란?
+    - 1번째 행, N번째 행, 1번째 열, M번째 열
+    - 위 4가지 중 하나 이상에 포함되는 칸을 의미
+- 지도의 테두리와 위 기준에 포함되지 않는 모든 칸은 땅을 의미
+- 임의의 두 좌표로 만들어지는 직사각형 모양의 범위에 새건물을 지을려고 한다.
+    - 직사각형에 포함되는 모든 칸에 건물이 포함되지 않아야 한다.
+"""
+def find_house():
+    global zip
+    delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    for i in range(N + 1):
+        for j in range(M + 1):
+            if zido[i][j] == 1:
+                zip.append((i, j))
+            elif zido[i][j] == 0:
+                for dx, dy in delta:
+                    nx, ny = i + dx, j + dy
+                    if 0 <= nx < N + 1 and 0 <= ny < M + 1:
+                        if zido[nx][ny] == 1:
+                            zip.append((i, j))
+
+
+N, M = map(int, input().split())
+zido = [[0] * (M + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    row = list(input())
+    for j in range(1, M + 1):
+        zido[i][j] = int(row[j - 1])
+
+Q = int(input())
+coordinate = []
+for _ in range(Q):
+    r1, c1, r2, c2 = map(int, input().split())
+    coordinate.append((r1, c1, r2, c2))
+
+zip = []
+find_house()
+print(len(zip))
+print(zip)
