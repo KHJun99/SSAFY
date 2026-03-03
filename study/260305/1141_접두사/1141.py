6e37d26de6509d2bed8a6720b5ade14fe684aaf5
KHJun99 <wnsk0524@naver.com>
2026-03-04 01:46:08 +0900

    study_260305

--- study/260305/1141_접두사/1141.py
+++ study/260305/1141_접두사/1141.py
@@ -13,3 +13,24 @@
 - 단어 N개로 이루어진 집합이 주어졌을 때,
   접두사 X 집합인 부분집합의 최대 크기 출력
 """
+N = int(input())
+
+words = set()
+for _ in range(N):
+    word = input().strip()
+    words.add(word)
+
+sorted_words = sorted(list(words), key=lambda x: len(x))
+M = len(sorted_words)
+
+remove = set()
+for idx1 in range(M):
+    for idx2 in range(idx1 + 1, M):
+        word, length = sorted_words[idx1], len(sorted_words[idx1])
+
+        if sorted_words[idx2][0:length] == word:
+            remove.add(word)
+
+result = [w for w in sorted_words if w not in remove]
+
+print(len(result))
